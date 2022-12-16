85fc887bec8b46e1134c6cd2eff43b9e12fb23c3
Malak Joseph <malakjoseph2010@gmail.com>
2022-12-17 00:46:10 +0200

    safely remove thumb folder

--- src/utils/fileUtils.ts
+++ src/utils/fileUtils.ts
@@ -1,10 +1,14 @@
 import path from "path";
+import { existsSync } from "fs";
+import { mkdir } from "fs/promises";
 
 export function generatePath(
   filename: string,
   width?: number,
   height?: number
 ): string {
+  createThumbFolder();
+
   if (!width && !height) {
     return path.join(__dirname, "../../assets/full", `${filename}.jpg`);
   }
@@ -14,4 +18,12 @@ export function generatePath(
     "../../assets/thumb",
     `${filename}-${width}x${height}.jpg`
   );
+}
+
+function createThumbFolder() {
+  const thumbDir = path.join(__dirname, "../../assets/thumb");
+
+  if (!existsSync(thumbDir)) {
+    mkdir(thumbDir, { recursive: true });
+  }
 }
